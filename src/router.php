d30f9755b11e94d7520b3f12aa6b02ab527992ba
tcoch <tcochard@outlook.com>
2025-08-08 16:33:39 +0200

    Add support to automatically register routes for procedural scripts

--- src/router.php
+++ src/router.php
@@ -4,7 +4,6 @@ return [
     "/testRouting" => [new \App\Controller\NotProcedural(), 'route'],
     "/testOtherRouting" => [new \App\Controller\NotProcedural(), 'otherRoute'],
     "/Procedural" => 'Procedural.php',
-    "/Procedural.php" => "Procedural.php",
     "/DirectAccessRoute" => '../../public/DirectAccess.php',
     '/ClassRedirect' => [new \App\Controller\HandleRedirect(), 'redirect'],
     '/ErrorOnRedirect' => [new \App\Controller\HandleRedirect(), 'unprocessableRedirect'],
