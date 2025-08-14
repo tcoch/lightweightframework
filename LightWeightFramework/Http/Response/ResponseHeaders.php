c4a72f6ecd50fc40261cd1c0debadae8fd18cf2a
Thomas Cochard <95548437+tcoch@users.noreply.github.com>
2025-08-14 14:15:16 +0200

    Better handling of HTTP headers (#6)

--- LightWeightFramework/Http/Response/ResponseHeaders.php
+++ LightWeightFramework/Http/Response/ResponseHeaders.php
@@ -4,24 +4,78 @@ namespace LightWeightFramework\Http\Response;
 
 class ResponseHeaders
 {
+    public const HEADER_LOCATION = 'Location';
+    public const HEADER_CONTENT_TYPE = 'Content-Type';
+
     public int $statusCode;
 
-    public ?string $location;
+    public ?string $location = null;
+
+    public array $headers = [];
 
     public function sendHeaders(): void
     {
+        // Update headers based on latest informations (updated location for example)
+        $this->calculateHeaders();
+
         switch ($this->statusCode) {
             case 200:
                 header("HTTP/1.0 200 OK");
-                header("Content-Type: text/html; charset=utf-8");
+                $this->sendContentTypeHeader();
                 break;
             case 302:
                 header("HTTP/1.0 302 Found");
-                header('Location: ' . $this->location);
+                $this->sendLocationHeader();
                 break;
             case 404:
                 header("HTTP/1.0 404 Not Found");
                 break;
+        }
+    }
+
+    private function calculateHeaders(): void
+    {
+        $this->calculateLocationHeader();
+        $this->calculateContentTypeHeader();
+    }
+
+    private function sendLocationHeader(): void
+    {
+        if (\array_key_exists(self::HEADER_LOCATION, $this->headers)) {
+            header($this->headers[self::HEADER_LOCATION]);
+        }
+    }
+
+    private function sendContentTypeHeader(): void
+    {
+        if (\array_key_exists(self::HEADER_CONTENT_TYPE, $this->headers)) {
+            header($this->headers[self::HEADER_CONTENT_TYPE]);
+        }
+    }
+
+    private function calculateLocationHeader(): void
+    {
+        foreach (headers_list() as $header) {
+            if (str_contains($header, 'Location:')) {
+                $this->headers[self::HEADER_LOCATION] = $header;
+            }
+        }
+
+        if (!\array_key_exists(self::HEADER_LOCATION, $this->headers) && $this->location) {
+            $this->headers[self::HEADER_LOCATION] = 'Location: ' . $this->location;
+        }
+    }
+
+    private function calculateContentTypeHeader(): void
+    {
+        foreach (headers_list() as $header) {
+            if (\str_contains(strtoupper($header), strtoupper('Content-Type'))) {
+                $this->headers[self::HEADER_CONTENT_TYPE] = $header;
+            }
+        }
+
+        if (!\array_key_exists(self::HEADER_CONTENT_TYPE, $this->headers)) {
+            $this->headers[self::HEADER_CONTENT_TYPE] = "Content-Type: text/html; charset=utf-8";
         }
     }
 }
---
+++ tests/Request/ResponseHeadersTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Tests\Request;
+
+use LightWeightFramework\Http\Request\Request;
+use LightWeightFramework\Http\Response\Response;
+use LightWeightFramework\LightWeightFramework;
+use LightWeightFramework\Routing\RouteCollection;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * cURL request is mandatory / cannot simulate request and response here
+ * Simulating request does no send headers
+ */
+class ResponseHeadersTest extends TestCase
+{
+    public function testNormalContentType(): void
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/Procedural.php");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_exec($ch);
+        $headers = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        self::assertSame("text/html; charset=utf-8", $headers);
+    }
+
+    public function testContentTypeForced(): void
+    {
+        $phpFileContent = <<<EOD
+<?php
+
+header("Content-Type: application/json");
+
+echo __DIR__;
+EOD;
+
+        $fileName = "testContentTypeForced.php";
+        $filePath = __DIR__ . "/../../src/Controller/$fileName";
+        file_put_contents($filePath, $phpFileContent);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/testContentTypeForced.php");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_exec($ch);
+        $headers = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        self::assertSame("application/json", $headers);
+
+        unlink($filePath);
+    }
+
+    public function testContentTypeForcedDifferentCase(): void
+    {
+        $phpFileContent = <<<EOD
+<?php
+
+header("Content-type: application/json");
+
+echo __DIR__;
+EOD;
+
+        $fileName = "testContentTypeForced.php";
+        $filePath = __DIR__ . "/../../src/Controller/$fileName";
+        file_put_contents($filePath, $phpFileContent);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://localhost/testContentTypeForced.php");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_exec($ch);
+        $headers = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        self::assertSame("application/json", $headers);
+
+        unlink($filePath);
+    }
+}
